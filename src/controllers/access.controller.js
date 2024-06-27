ea069a32e8ff47fccb6efd7a7fb3baf7339a381c
ViLe <lecongvi18052002@gmail.com>
2024-06-27 21:00:00 +0700

    Make API success response

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,11 +1,19 @@
 'use strict';
 
+const { OK, CREATED } = require("../core/success.response");
 const AccessService = require("../services/access.service");
+
 
 class AccessController {
     // sign up controller
     signUp = async(req, res, next) => {
-        return res.status(201).json(await AccessService.signUp(req.body));
+        new CREATED({
+            message: 'Registered OK!',
+            metadata: await AccessService.signUp(req.body),
+            options: {
+                limit: 10
+            }
+        }).send(res)
     };
 }
 
