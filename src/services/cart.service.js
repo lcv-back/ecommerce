2498ddbcef611c692986b9123c3a6d06359fd081
ViLe <lecongvi18052002@gmail.com>
2024-09-13 00:58:23 +0700

    Add cart model

---
+++ src/services/cart.service.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/*
+    Key Features
+    - Add product to cart [User]
+    - Reduce product quantity by one [User]
+    - Increase product quantity by one [User]
+    - Get cart [User]
+    - Delete cart [User]
+    - Delete cart item [User]
+*/
+
+class CartService {
+
+    static async addToCart({ userId, productId = {} }) {
+        // check cart is contain?
+
+    }
+}
+
+module.exports = CartService;
