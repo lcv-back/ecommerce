f4d6d36aa8bf6a254cad4fff8691556000ea98d1
Vi Le <lecongvi18052002@gmail.com>
2024-12-23 12:17:12 +0700

    Fix name comment content

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -8,7 +8,7 @@ const COLLECTION_NAME = 'Comments'
 const commentSchema = new Schema({
     comment_productId: { type: Schema.Types.ObjectId, ref: 'Product' },
     comment_userId: { type: Number, default: 1 },
-    commnet_content: { type: String, default: 'text' },
+    comment_content: { type: String, default: 'text' },
     comment_left: { type: Number, default: 0 },
     comment_right: { type: Number, default: 0 },
     comment_parentId: { type: Schema.Types.ObjectId, ref: DOCUMENT_NAME },
